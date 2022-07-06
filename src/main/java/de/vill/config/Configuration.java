4a90ebc3d1a9573f7473d2f4cfc2abce83899ce5
Stefan Vill <76208574+st-vi@users.noreply.github.com>
2022-07-06 14:39:00 +0200

    doc: added TODO for configuration

--- src/main/java/de/vill/config/Configuration.java
+++ src/main/java/de/vill/config/Configuration.java
@@ -1,6 +1,7 @@
 package de.vill.config;
 
 public class Configuration {
+    //TODO config sollte man nicht nur hardcoded sondern auch über Methodenaufruf in der Bibliothek nutzen können. Vielleicht diese Config einfach in der UVLParse Klasse halten und setzen lassen, die das Interface nach außen für parse und print hält?
     public final static String NEWLINE = "\n";
     public final static String TABULATOR = "        ";
 }
