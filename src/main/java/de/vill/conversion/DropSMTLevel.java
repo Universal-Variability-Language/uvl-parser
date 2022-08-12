b78c38e4c02e3165d082bcee744a960d80cf609a
Stefan Vill <76208574+st-vi@users.noreply.github.com>
2022-08-12 15:20:34 +0200

    feat: implemented algorithm to drop SMT level

--- src/main/java/de/vill/conversion/DropSMTLevel.java
+++ src/main/java/de/vill/conversion/DropSMTLevel.java
@@ -2,22 +2,41 @@ package de.vill.conversion;
 
 import de.vill.model.FeatureModel;
 import de.vill.model.LanguageLevel;
+import de.vill.model.constraint.Constraint;
+import de.vill.model.constraint.ExpressionConstraint;
+import de.vill.model.expression.Expression;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class DropSMTLevel implements IConversionStrategy{
     @Override
     public Set<LanguageLevel> getLevelsToBeRemoved() {
-        return null;
+        return new HashSet<>(Arrays.asList(LanguageLevel.SMT_LEVEL));
     }
 
     @Override
     public Set<LanguageLevel> getTargetLevelsOfConversion() {
-        return null;
+        return new HashSet<>();
     }
 
     @Override
     public void convertFeatureModel(FeatureModel featureModel) {
+        featureModel.getOwnConstraints().removeIf(x -> constraintContainsEquation(x));
+    }
 
+    private boolean constraintContainsEquation(Constraint constraint){
+        if(constraint instanceof ExpressionConstraint){
+            return true;
+        }else {
+            for (Constraint subConstraint : constraint.getConstraintSubParts()){
+                if(constraintContainsEquation(subConstraint)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
