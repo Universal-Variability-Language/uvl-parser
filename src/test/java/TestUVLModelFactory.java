96baa682e7bc06b618c98c9518b16cf203d736a2
Stefan Vill <76208574+st-vi@users.noreply.github.com>
2022-08-14 15:39:43 +0200

    feat: added basic unit test for single feature model printing

--- src/test/java/TestUVLModelFactory.java
+++ src/test/java/TestUVLModelFactory.java
@@ -1,6 +1,7 @@
 import de.vill.config.Configuration;
 import de.vill.main.UVLModelFactory;
 import de.vill.model.FeatureModel;
+import de.vill.model.Import;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -30,6 +31,29 @@ public class TestUVLModelFactory {
             }
 
             //assertEquals(content, featureModel.composedModelToString());
+        }catch (IOException e){
+            fail();
+        }
+    }
+
+    @Test
+    public void testPrintSingleModel() {
+        try {
+            Configuration.setNewlineSymbol("\n");
+            Configuration.setTabulatorSymbol("\t");
+            Path filePath = Path.of("./src/test/resources/composition_sub1.uvl");
+            String content = Files.readString(filePath);
+            UVLModelFactory uvlModelFactory = new UVLModelFactory();
+            FeatureModel featureModel = uvlModelFactory.parse(content, "./src/test/resources/");
+            var uvlStrings = featureModel.decomposedModelToString();
+
+            for(Import importLine : featureModel.getImports()){
+                if(importLine.getFeatureModel().getNamespace().equals("composition_sub1")){
+                    assertEquals(importLine.getFeatureModel().toString(), content);
+                }
+                break;
+            }
+
         }catch (IOException e){
             fail();
         }
