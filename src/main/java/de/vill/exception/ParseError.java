6df59ea5a6bd26bdfa806073cbc9d60be91aea60
Stefan Vill <76208574+st-vi@users.noreply.github.com>
2022-09-01 10:02:42 +0200

    feat: added line attribute for ParseError

--- src/main/java/de/vill/exception/ParseError.java
+++ src/main/java/de/vill/exception/ParseError.java
@@ -1,8 +1,15 @@
 package de.vill.exception;
 
 public class ParseError extends RuntimeException{
+
+    public int getLine() {
+        return line;
+    }
+
+    private int line = 0;
     public ParseError(int line, int charPositionInLine, String errorMessage, Throwable err) {
         super(errorMessage, err);
+        this.line = line;
     }
     public ParseError(String errorMessage) {
         super(errorMessage);
