e1142465331f4ff19742f24c1f5733b451d06880
Chico Sundermann <chico.sundermann@uni-ulm.de>
2023-06-06 22:21:30 +0200

    Fixes #17: Correctly convert non-boolean features

--- src/main/java/de/vill/util/Constants.java
+++ src/main/java/de/vill/util/Constants.java
@@ -1,9 +1,13 @@
 package de.vill.util;
 
 public class Constants {
+    // Literal Types
     public static final String NUMBER = "number";
     public static final String STRING = "string";
     public static final String BOOLEAN = "boolean";
     public static final String TRUE = "true";
     public static final String UNDEF = "undef";
+
+    // Default attribtues
+    public static final String TYPE_LEVEL_VALUE = "type_level_value";
 }
