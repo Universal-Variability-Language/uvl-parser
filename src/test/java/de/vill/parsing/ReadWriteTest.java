fbb8ba6484804da200cb53fd4a89d8adfb1dc377
Chico Sundermann <chico.sundermann@uni-ulm.de>
2023-07-19 19:35:04 +0200

    Added many test cases for parsing uvl models (#27)

--- src/test/java/de/vill/parsing/ReadWriteTest.java
+++ src/test/java/de/vill/parsing/ReadWriteTest.java
@@ -1,4 +1,4 @@
-package de.vill.model;
+package de.vill.parsing;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -6,11 +6,12 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 import de.vill.main.UVLModelFactory;
+import de.vill.model.FeatureModel;
 
 public class ReadWriteTest {
     
     private final static String TEMP_PATH = "temp.uvl";
-    private final static String TEST_MODEL_ONE = "src/test/resources/test1.uvl";
+    private final static String TEST_MODEL_ONE = "src/test/resources/parsing/bike.uvl";
 
     @Test
     void testReadWrite() throws Exception {
@@ -19,13 +20,15 @@ public class ReadWriteTest {
         // Read
         String content = new String(Files.readAllBytes(Paths.get(TEST_MODEL_ONE)));
         FeatureModel featureModel = uvlModelFactory.parse(content);
+        assert(featureModel != null);
 
         // Write
         Files.write(Paths.get(TEMP_PATH), featureModel.toString().getBytes());
 
         // Try reading again
         content = new String(Files.readAllBytes(Paths.get(TEMP_PATH)));
-        uvlModelFactory.parse(content);
+        FeatureModel reFeatureModel = uvlModelFactory.parse(content);
+        assert(reFeatureModel != null);
         Files.delete(Paths.get(TEMP_PATH));
     }
 }
